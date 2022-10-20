eaa4dddadc95d9165c274cb5d19bbf49b0c959c7
Anastasyia <moya_pochta_n.k@mail.ru>
2022-10-20 12:40:40 +0700

    Task 06

---
+++ Task_06/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int N = 10;
+int[] array = new int[N];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.WriteLine(array[i] + " ");
+}
+Console.WriteLine();
+int j = array.Length - 1;
+for (int i = 0; i < (array.Length + 1) / 2; i++)
+{
+    Console.WriteLine(array[i] * array[j] + " ");
+    j--;
+}
